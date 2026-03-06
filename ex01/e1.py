98702b3f91765560f7a110c259472b34d1801339
Paul Troeger <41645798+paultroeger@users.noreply.github.com>
2026-03-06 14:25:33 +0100

    Added exercise 1

---
+++ ex01/e1.py
@@ -0,0 +1,44 @@
+# Load the data and libraries
+import pandas as pd
+import numpy as np
+import scipy.stats
+import pytest
+
+
+def is_k_anonymous(k, qis, df):
+    """Returns true if df satisfies k-Anonymity for the quasi-identifiers 
+    qis. Returns false otherwise."""
+    # TODO: your code here
+    raise NotImplementedError()
+
+
+def generalize_categorical():
+    # TODO: your code here
+    raise NotImplementedError()
+
+
+def generalize_numeric(zip, n):
+    # TODO: your code here
+    raise NotImplementedError()
+
+
+def suppress_count(k, qis, df):
+    # TODO: your code here
+    raise NotImplementedError()
+
+
+def is_l_diverse(l, qis, sens_col, df, type='probabilistic'):
+    # The type parameter has two valid values: 'probabilistic' and 'entropy'
+    # TODO: your code here
+    raise NotImplementedError()
+
+
+def max_l(qis, sens_col, df, type='probabilistic'):
+    # The type parameter has two valid values: 'probabilistic' and 'entropy'
+    # TODO: your code here
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    # TODO: your code here
+    raise NotImplementedError()
