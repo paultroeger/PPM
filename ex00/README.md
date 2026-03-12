10811d47dcb7041c1c2a0f629ed464cb7241c035
Paul Troeger <41645798+paultroeger@users.noreply.github.com>
2026-03-12 17:50:44 +0100

    Added some work on Ex0

---
+++ ex00/README.md
@@ -0,0 +1,34 @@
+# HOW TO RUN
+
+# Task Questions
+
+## Task 1
+### What does the data mean in each column mean?
+| Row Name | Meaning |
+| - | - |
+| Name | Person Name |
+| DOB | Date of Birth |
+| SSN | Social Security Number |
+| Zip | Zip Code | 
+| Age | Age |
+| Workclass | Work Field |
+| fnlwgt | Final Weight (Amount of People represented) |
+| Education | Education |
+| Education-Num | Education as expressed by numbers |
+| Martial Status | Martial Status |
+| Occupation | Occupation |
+| Relationship | Relationship Status |
+| Race | Race |
+| Sex | Gender |
+| Capital Gain | Profit from Capital Assets |
+| Capital Loss | Loss from Capital Assets |
+| Hours per week | Work hours per week |
+| Country | Country |
+| Target | Income Target | 
+
+## Task 2
+### What columns would you remove?
+I would remove Name and SSN because they uniquely identify a person instantly.
+### Why these columns and not the others?
+I wouldn't remove the others because taken on their own they don't uniquely identify a person.
+For example many people can be born on the same day and without other knowledge it would be impossible to identify one person from that information.
--- ex00/e0.py
+++ ex00/e0.py
@@ -5,23 +5,33 @@ import pytest
 
 
 def load_adult():
-    # TODO: your code here
-    raise NotImplementedError()
+    return pd.read_csv('adult_with_pii.csv')
 
 
 def print_stats(data):
-    # TODO: your code here
-    raise NotImplementedError()
+    print("--- Columns ---")
+    print(data.columns, '\n')
+
+    print("--- Head ---")
+    pd.set_option('display.max_columns', None)
+    print(data.head(), '\n')
+    pd.reset_option('display.max_columns')
+    
+    print("--- Row Size ---")
+    print(data.size, '\n')
+
+    print("--- Missing Values ---")
+    has_missing = data.isna().values.any()
+    print(f"Contains missing values: {has_missing}")
+    print(data.isna().sum(), '\n')
 
 
 def deidentify_dataset(data):
-    # TODO: your code here
-    raise NotImplementedError()
+    return data.drop(columns=['Name', 'SSN'])
 
 
 def get_rosys_row(data):
-    # TODO: your code here
-    raise NotImplementedError()
+    return data[data['Name'].str.contains('Rosy')].iloc[0]
 
 
 def test_get_rosys_row():
@@ -30,8 +40,7 @@ def test_get_rosys_row():
 
 
 def recover_rosys_row(data, data_deid):
-    # TODO: your code here
-    raise NotImplementedError()
+    return data_deid[(data_deid['Age'] == data['Age']) & (data_deid['Zip'] == data['Zip'])].iloc[0]
 
 
 def test_recover_rosys_row():
@@ -40,8 +49,7 @@ def test_recover_rosys_row():
 
 
 def group_one_count(data, col):
-    # TODO: your code here
-    raise NotImplementedError()
+    return data.groupby(col).size().reset_index(name='count')
 
 
 def test_group_one_count():
@@ -50,13 +58,11 @@ def test_group_one_count():
 
 
 def group_one_age(data, col):
-    # TODO: your code here
-    raise NotImplementedError()
+    return data.groupby(col)['Age'].mean().reset_index()
 
 
 def group_two_count(data, col1, col2):
-    # TODO: your code here
-    raise NotImplementedError()
+    return data.groupby([col1, col2]).size().reset_index(name='count')
 
 
 def test_group_two_count():
@@ -65,8 +71,7 @@ def test_group_two_count():
 
 
 def group_two_age(data, col1, col2):
-    # TODO: your code here
-    raise NotImplementedError()
+    return data.groupby([col1, col2])['Age'].mean().reset_index()
 
 
 def test_group_two_age():
@@ -75,25 +80,75 @@ def test_group_two_age():
 
 
 def get_rosys_age(data):
-    # TODO: your code here
-    raise NotImplementedError()
+    data_without_rosy = data[~data['Name'].str.contains('Rosy')]
+    return data['Age'].sum() - data_without_rosy['Age'].sum()
     
 
 def get_rosys_age_noname(data):
-    # TODO: your code here
-    raise NotImplementedError()
+    data_without_rosy = data[~(data['Zip'] == 75436)]
+    return (data['Age'].sum() - data_without_rosy['Age'].sum())
     
 
 def get_rosys_age_mean(data):
-    # TODO: your code here
-    raise NotImplementedError()
+    data_without_rosy = data[~data['Name'].str.contains('Rosy')]
+    return data['Age'].mean() * len(data) - data_without_rosy['Age'].mean() * len(data_without_rosy)
 
 
 def test_rosys_age():
     # TODO: your test cases here
     raise NotImplementedError()
 
+def hist_unique_entries(data):
+    print(f"Zip Uniqueness: {len(data['Zip'].unique())}")
+    print(f"Age Uniqueness: {len(data['Age'].unique())}")
+    print(f"DOB Uniqueness: {len(data['DOB'].unique())}")
+    print(f"\t\t/{len(data)}")
+
+    # histogram
+    zip_age_counts = data.groupby(['Zip', 'Age']).size()
+    zip_dob_counts = data.groupby(['Zip', 'DOB']).size()
+
+    plt.figure(figsize=(6, 6))
+
+    plt.hist([zip_age_counts, zip_dob_counts], bins=2, label=['Zip + Age', 'Zip + DOB'])
+
+    plt.title('Uniqueness Comparison: (Zip, Age) vs (Zip, DOB)')
+    plt.xlabel('Group Size')
+    plt.ylabel('Frequency')
+    plt.legend(loc='upper right')
+    max_val = max(zip_age_counts.max(), zip_dob_counts.max())
+    plt.xticks(range(1, max_val + 1))
+    plt.show()
+
+def hist_link_attack(data):
+    data_deid = deidentify_dataset(data)
+    
+    data_dob = pd.merge(data[['Name', 'DOB']], data_deid, on='DOB', how='left')
+    counts = data_dob['Name'].value_counts()
+    unique_names = counts[counts == 1].index.tolist()
+    data_dob_identified_n = len(data_dob[data_dob['Name'].isin(unique_names)])
+
+    data_dob_zip = pd.merge(data[['Name', 'DOB', 'Zip']], data_deid, on=['DOB', 'Zip'], how='left')
+    counts = data_dob_zip['Name'].value_counts()
+    unique_names = counts[counts == 1].index.tolist()
+    data_dob_zip_identified_n = len(data_dob_zip[data_dob_zip['Name'].isin(unique_names)])
+    
+    print(f"Identified by DOB: {data_dob_identified_n}")
+    print(f"Identified by DOB and Zip: {data_dob_zip_identified_n}")
+    print(f"Dataset size: {len(data)}")
+
+def counts(data):
+    print("--- Occupation ---")
+    print(pd.merge(group_one_count(data, 'Occupation'), group_one_age(data, 'Occupation'), on='Occupation'), '\n')
+    
+    print("--- Education ---")
+    print(pd.merge(group_one_count(data, 'Education'), group_one_age(data, 'Education'), on='Education'), '\n')
+
+    print("--- Occupation & Education---")
+    print(pd.merge(group_two_count(data, 'Occupation', 'Education'), group_two_age(data, 'Occupation', 'Education'), on=['Occupation', 'Education']), '\n')
 
 if __name__ == "__main__":
-    # TODO: your code here
-    raise NotImplementedError()
+    data = load_adult()
+    print(get_rosys_age(data))
+    print(get_rosys_age_noname(data))
+    print(get_rosys_age_mean(data))
