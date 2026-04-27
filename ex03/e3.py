a8b3a731ff8e5de8c432b59ecc7b353875b89c62
Paul Troeger <41645798+paultroeger@users.noreply.github.com>
2026-04-27 17:05:42 +0200

    Added Tests for T2 and wrote to answer_e3

--- ex03/e3.py
+++ ex03/e3.py
@@ -7,7 +7,6 @@ import pytest
 # %%
 
 # arbitrary
-MAX_INT = 100
 GLOBAL_SEED = 1234
 field=galois.GF(2 ** 13 - 1)
 
@@ -94,7 +93,7 @@ class BGW(Party):
         coeffs = get_lagrange_coeffs(p)
         y = field(0)
         for i in range(n):
-          y += coeffs[i](0) * self.shares[i][1]
+          y += coeffs[i] * self.shares[i][1]
         self.output = [self.shares[0][0], y]
 
 
@@ -118,7 +117,7 @@ def run_bgw(t, n, a, b):
     for i in range(n):
       outputs.append(parties[i].get_view())
     outputs = field(outputs)
-    print(reconstruct(outputs))
+    return reconstruct(outputs)
 
 
 def get_lagrange_coeffs(x_values):
@@ -129,22 +128,90 @@ def get_lagrange_coeffs(x_values):
         y_values = field.Zeros(n)
         y_values[i] = 1
      
-        p.append(galois.lagrange_poly(x_values, y_values))
+        p.append(galois.lagrange_poly(x_values, y_values)(0))
         
     return p
 
-# Tasks
+# Tests
 
+## Task 2
+
+def test_run_bgw_large_t():
+    t, n, a, b = 3, 5, 5, 5
+    with pytest.raises(Exception):
+      run_bgw(t, n, a, b)
+
+def test_run_bgw_correctness():
+    
+    t = [2, 2, 4]
+    n = [5, 5, 10]
+    a = [5, 6, 59]
+    b = [5, 1, 42]
+
+    for i in range(len(n)):
+        assert a[i] * b[i] == run_bgw(t[i], n[i], a[i], b[i])
+
+
+def test_get_lagrange_coeffs_two_points():
+    """
+    x = [1, 2]
+    L0(0) = (0 - x1) / (x0 - x1) = (0 - 2) / (1 - 2) = (0-2)/-1 = 2
+    L1(0) = (0 - x0) / (x1 - x0) = (0 - 1) / (2 - 1) = (0-1) = -1
+    """
+    x_values = field([1, 2])
+    
+    expected = [field(2), field(-1 % field.order)]
+    
+    result = get_lagrange_coeffs(x_values)
+    
+    assert result == expected
+    
+    # sum of lagrange coeffs must be 1
+    assert field(np.sum(result) % field.order) == field(1)
+    
+    # check if weighted sum is 0
+    sum = field(0)
+    for i in range(len(x_values)):
+        sum += result[i] * x_values[i]
+    assert sum == field(0)
+
+def test_get_lagrange_coeffs_three_points():
+    """
+    x = [1, 2, 3]
+    L0(0) = (0-2)(0-3) / (1-2)(1-3) = 6 / 2 = 3
+    L1(0) = (0-1)(0-3) / (2-1)(2-3) = 3 / -1 = -3
+    L2(0) = (0-1)(0-2) / (3-1)(3-2) = 2 / 2 = 1
+    """
+    x_values = field([1, 2, 3])
+    expected = [field(3), field(-3 % field.order), field(1)]
+    
+    result = get_lagrange_coeffs(x_values)
+    
+    assert result == expected
+    assert field(np.sum(result) % field.order) == field(1)
+
+    # check if weighted sum is 0
+    sum = field(0)
+    for i in range(len(x_values)):
+        sum += result[i] * x_values[i]
+    assert sum == field(0)
+
+#%% Tasks
 def task1():
     print('-' * 6, 'Task 1', '-' * 6)
-    secret1 = shamir_share(field(12), 1, 4)
-    secret2 = shamir_share(field(5), 1, 4)
-    print(secret1)
-    print(secret2)
-    secrets1 = add_const(secret1, 2)
+    t, n, a, b = 1, 4, 12, 5
+    print(f"t:{t}, n:{n}, a:{a}, b:{b}")
+    secret1 = shamir_share(field(a), t, n)
+    secret2 = shamir_share(field(b), t, n)
+    print("Secret1\n", secret1)
+    print("Secret2\n", secret2)
+    c = 2
+    print("Adding constant", c, "to shares1")
+    secret1 = add_const(secret1, c)
     reconstruction = reconstruct(secret1[0:2])
     print("re:", reconstruction)
 
+    print("Adding shares together..")
     secret3 = add_shares(secret1, secret2)
     reconstruction = reconstruct(secret3[0:2])
     print("re:", reconstruction)
@@ -154,12 +221,14 @@ def task2():
     print('-' * 6, 'Task 2', '-' * 6)
     t, n, a, b = 2, 5, 5, 5
     print(f"t:{t}, n:{n}, a:{a}, b:{b}")
-    run_bgw(t, n, a, b)
+    print("re:", run_bgw(t, n, a, b))
     print()
 
+#%%
 if __name__ == "__main__":
-    #field = galois.GF(2 ** 13 - 1)
+    task1()
     task2()
+    pass
 
 
 # %%
